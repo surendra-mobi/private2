de1054d0ba0857c8a5da761675424456f8bb73f9
unknown <kumarsm2405@gmail.com>
2019-06-19 09:34:50 +0530

    ang

---
+++ server/config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DB_URI:"mongodb://surendra:surendra91@ds135456.mlab.com:35456/ang7",
+  SECRET: "sksksks",
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  STRIPE_SK: process.env.STRIPE_SK
+}
+
+
